1f2b3170d0354fcf2d3caf1d993619aa1f10a0a3
Zubair1020 <rakibhossianraju@gmail.com>
2023-12-31 02:45:00 +0600

    Added the banner section on the home page

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,42 @@
+import {
+  banner,
+  heading,
+  subHeading1,
+  subHeading2,
+  contentBox,
+  description,
+  // bannerImgContainer,
+  bannerImg,
+} from "./HomePage.module.css";
+import ButtonComponent from "../../components/button/ButtonComponent.jsx";
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => {
-  return <div>I am home page component</div>;
+  const navigate = useNavigate();
+  return (
+    <header className={banner}>
+      <div className={contentBox}>
+        <h1 className={heading}>
+          <span className={subHeading1}>Your Halal Career </span>
+          <span className={subHeading2}>Journey Starts Here</span>
+        </h1>
+        <p className={description}>
+          Get you dream job with Islamic culture in your work environment.
+          Settle your <span>Din</span> and <span>Duniya</span>.
+        </p>
+        <ButtonComponent onClick={() => navigate("/jobs")}>
+          Explore Jobs
+        </ButtonComponent>
+      </div>
+      {/*<div className={bannerImgContainer}>*/}
+      <img
+        className={bannerImg}
+        src="/assets/images/background.svg"
+        alt="Person svg"
+      />
+      {/*</div>*/}
+    </header>
+  );
 };
 
 export default HomePage;
